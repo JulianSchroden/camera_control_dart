6e4faa01f7b2a1deff5486c1c18bc48017ffb8df
Julian Schroden <juschroden@gmail.com>
2023-08-21 19:26:26 +0200

    Init project

---
+++ example/camera_control_dart_example.dart
@@ -0,0 +1,6 @@
+import 'package:camera_control_dart/camera_control_dart.dart';
+
+void main() {
+  var awesome = Awesome();
+  print('awesome: ${awesome.isAwesome}');
+}
---
+++ test/camera_control_dart_test.dart
@@ -0,0 +1,16 @@
+import 'package:camera_control_dart/camera_control_dart.dart';
+import 'package:test/test.dart';
+
+void main() {
+  group('A group of tests', () {
+    final awesome = Awesome();
+
+    setUp(() {
+      // Additional setup goes here.
+    });
+
+    test('First Test', () {
+      expect(awesome.isAwesome, isTrue);
+    });
+  });
+}
